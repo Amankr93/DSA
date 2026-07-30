5ef3f34f5cade06849617b09e184c49536739469
Aman Kumar <amankumar523008@gmail.com>
2026-07-30 15:16:20 +0530

    Time: 1 ms (45.64%) | Memory: 77.3 MB (13.14%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -15,8 +15,22 @@ public:
         
         ListNode* head = res;
         int carry = 0;
-        while(l1!=NULL && l2!=NULL){
-            int value = l1->val + l2->val +carry;
+        while(l1!=NULL || l2!=NULL){
+            int value= 0;
+            if(l1 && l2){
+                value= l1->val + l2->val + carry;
+                l1=l1->next;
+                l2=l2->next;
+            }
+            else if(l1 && !l2){
+                value= l1->val + carry;
+                l1=l1->next;
+            }
+            else{
+                value=  l2->val + carry;
+                l2=l2->next;
+            }
+            
             carry = value/10;
             value= value%10;
             
@@ -25,36 +39,9 @@ public:
             head = head->next;
             head->val = value;
             
-            l1=l1->next;
-            l2=l2->next;
+           
         }
-        while(l1!=NULL){
-            int value = l1->val + carry;
-            carry = value/10;
-            value= value%10;
-            
-            ListNode* node = new ListNode();
-            head->next = node;
-            head = head->next;
-
-            head->val = value;
-            
-            l1=l1->next;
-            
-        }
-        while(l2!=NULL){
-            int value = l2->val + carry;
-            carry = value/10;
-            value= value%10;
-            
-            ListNode* node = new ListNode();
-            head->next = node;
-            head = head->next;
-
-            head->val = value;
-            l2=l2->next;
-            
-        }
+        
         if(carry){
             ListNode* node = new ListNode();
             head->next = node;
