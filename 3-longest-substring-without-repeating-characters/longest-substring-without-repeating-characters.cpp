ca14e60cd7d2b7519deaa31029966eadc1ae0797
Aman Kumar <amankumar523008@gmail.com>
2026-07-30 19:23:49 +0530

    Time: 184 ms (5.92%) | Memory: 140.2 MB (5.05%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
 int lengthOfLongestSubstring(string s) {
         int maxLength = 0;
-        for (int i=0;i<s.length();i++){
-            string newString ="";
-            
-            for(int j=i; j<s.length();j++){
-                // cout<<s[j]<< " "<<newString<<endl;
-                if(newString.find(s[j])!=std::string::npos){
-                    int length = newString.length();
-                    maxLength = max(maxLength, length);
-                    // cout<<newString<<"at the end "<<length<< endl;
-                    break;
-
-                }
-                newString.push_back(s[j]);
+        int i=0;
+        string newString ="";
+        while (i<s.length()){
+            if(newString.find(s[i])!=string::npos){
                 int length = newString.length();
-                    maxLength = max(maxLength, length);
+                maxLength =max(maxLength, length);
+                int index = newString.find(s[i]);
+                
+                newString =index+1<newString.length()? newString.substr(index+1): "";
+                
+                newString.push_back(s[i]);
+                i++;
+            }
+            else{
+                newString.push_back(s[i]);
+                int length = newString.length();
+
+                maxLength =max(maxLength, length);
+                i++;
 
             }
+            
+            
         }
+         
         return maxLength;
         
     }
