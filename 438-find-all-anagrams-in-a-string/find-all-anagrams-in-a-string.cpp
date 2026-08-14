534b573c4fb6a431a0e4c9123e9c024913d18b66
Aman Kumar <amankumar523008@gmail.com>
2026-08-14 07:36:08 +0530

    Time: 7 ms (48.65%) | Memory: 11.5 MB (63.67%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,40 +1,64 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        map<char, int> freq1;
-        map<char, int> freq2;
+        // map<char, int> freq1;
+        // map<char, int> freq2;
         int m = s.length();
         int n = p.length();
-        if(m<n)return {};
-        for(int i = 0 ; i<p.length();i++){
-            freq1[p[i]]++;
-            freq2[s[i]]++;
-
-        }
-        int i=0; 
         vector<int>ans;
-        for(int j=p.length()-1;j<s.length();j++){
-            int k=i;
-           
-            bool found = true;
-            while(k<=j){
-                if(freq1[s[k]]!=freq2[s[k]])
-                {
-                    found = false;
-                    break;
-                }
-                k++;
+        if(m<n)return ans;
+        map<char,int>mp;
+        for(auto ch:p){
+            mp[ch]++;
+        }
+        int count = n;
+        int i=0;
+        for(int j = 0;j<m;j++){
+            if(mp[s[j]]>0){
+                count--;
             }
-            if(found){
+            mp[s[j]]--;
+            if(j-i+1> n){
+                if(mp[s[i]]>=0){
+                    count++;
+                }
+                mp[s[i]]++;
+                i++;
+            }
+            if(count == 0&& j-i+1 == n){
                 ans.push_back(i);
             }
-            // while(k<=j){
-            //     if()
-            // }
-            freq2[s[i]]--;
-            if(j+1<m)freq2[s[j+1]]++;
-            i++;
         }
+        // for(int i = 0 ; i<p.length();i++){
+        //     freq1[p[i]]++;
+        //     freq2[s[i]]++;
+        // }
+        // // int i=0; 
+        // sort(p.begin(), p.end());
+        
+        // for(int j=p.length()-1;j<s.length();j++){
+            // int k=i;
+            // bool found = true;
+            // while(k<=j){
+            //     if(freq1[s[k]]!=freq2[s[k]])
+            //     {
+            //         found = false;
+            //         break;
+            //     }
+            //     k++;
+            // }
+            // if(found){
+            //     ans.push_back(i);
+            // }
+            // // while(k<=j){
+            // //     if()
+            // // }
+            // freq2[s[i]]--;
+            // if(j+1<m)freq2[s[j+1]]++;
+            // i++;
+        
+        // }
+
         return ans;
         
     }
