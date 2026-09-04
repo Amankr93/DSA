01536e588be887fc7d19ac8cdb5eefd3bfc2a7c4
Aman Kumar <amankumar523008@gmail.com>
2026-09-04 12:19:33 +0530

    Time: 62 ms (28.49%) | Memory: 24 MB (25.04%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,18 +8,40 @@
  */
 class Solution {
 public:
+ListNode* solve(ListNode* head1, ListNode* head2, int diff){
+      ListNode* curr2 = head2;
+      while(diff>0){
+          curr2 = curr2->next;
+          diff--;
+      }
+      ListNode* curr1= head1;
+      while(curr1 && curr2){
+          if(curr1==curr2)return curr1;
+          curr1 = curr1->next;
+          curr2  = curr2->next;
+      }
+      return NULL;
+}
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*, bool> visited;
+       int count  = 0;
+        int count2 = 0;
         ListNode* curr1 = headA;
         ListNode* curr2 = headB;
-        while(curr1){
-            visited[curr1]=true;
-            curr1 = curr1->next;
+        while(curr1 || curr2){
+            if(curr1){
+                count++;
+                curr1 = curr1->next;
+            }
+            if(curr2){
+                count2++;
+                curr2 = curr2->next;
+            }
         }
-        while(curr2){
-            if(visited[curr2])return curr2;
-            curr2 = curr2->next;
+        // return headA;
+        if(count<=count2){
+            return solve(headA, headB, count2-count);
         }
-        return NULL;
+        return solve(headB, headA, count-count2);
     }
 };
